2b90bf4344e953c907835852e460406fe60f2f12
malla-lokesh <121810307046@gitam.in>
2024-06-03 07:50:21 +0530

    Fixed styles of dialog component

--- src/UI/Dialog/index.jsx
+++ src/UI/Dialog/index.jsx
@@ -2,7 +2,11 @@ import { forwardRef } from "react";
 import styles from "../styles/Dialog.module.css";
 
 const Dialog = forwardRef((props, ref) => {
-  return <dialog ref={ref}>{props.children}</dialog>;
+  return (
+    <dialog ref={ref} className={styles.dialog}>
+      {props.children}
+    </dialog>
+  );
 });
 
 export default Dialog;
