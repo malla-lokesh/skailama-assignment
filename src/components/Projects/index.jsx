53c02fd0b3bb4198f76888d7c4b52528d689df9a
malla-lokesh <121810307046@gitam.in>
2024-06-04 07:59:35 +0530

    fetching projects from backend

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,41 +1,60 @@
 import Button from "../../UI/Button/Button";
 import styles from "../../styles/Projects.module.css";
 import ProjectCard from "./ProjectCard";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useRef } from "react";
+import Dialog from "../../UI/Dialog";
 
 const Projects = () => {
+  const location = useLocation();
+  const { projects } = location?.state || {};
+  console.log(projects);
+
+  const dialogRef = useRef(null);
+
+  const handleOpenDialog = () => {
+    if (dialogRef.current) dialogRef.current.showModal();
+  };
+
+  const handleCloseDialog = () => {
+    if (dialogRef.current) dialogRef.current.close();
+  };
+
+  const handleCreateProjectDialog = () => {
+    // write logic to create project
+  };
+
   return (
     <div className={styles.projectsSection}>
       <div className={styles.headerSection}>
         <div className={styles.projectsTitle}>Projects</div>
-        <Button text={"Create New Project"} />
+        <Button text={"Create New Project"} openDialog={handleOpenDialog} />
       </div>
+      <Dialog ref={dialogRef}>
+        <h2>Create Project</h2>
+        <label htmlFor="project-name">Enter Project Name:</label>
+        <input type="text" id="project-name" placeholder="Type here" />
+        <div className={styles.btnGroup}>
+          <button className={styles.cancelBtn} onClick={handleCloseDialog}>
+            Cancel
+          </button>
+          <button
+            className={styles.createBtn}
+            onClick={handleCreateProjectDialog}
+          >
+            Create
+          </button>
+        </div>
+      </Dialog>
       <div className={styles.projectsGrid}>
-        <Link to={"/uploads"}>
-          <ProjectCard />
-        </Link>
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+        {projects?.map((project) => {
+          console.log(project);
+          return (
+            <Link to={"/uploads"} key={project.projectId}>
+              <ProjectCard project={project} />
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
